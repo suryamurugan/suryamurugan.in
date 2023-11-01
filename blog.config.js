5a268edc3f555fa37c1cc4c26d6ff15a4d23663f
Surya M <surya.m@phonepe.com>
2023-11-01 13:15:12 +0530

    base init

--- blog.config.js
+++ blog.config.js
@@ -1,7 +1,7 @@
 const BLOG = {
-  title: 'CRAIGARY',
-  author: 'Craig Hart',
-  email: 'i@craigary.net',
+  title: 'SURYA',
+  author: 'Surya Murugan',
+  email: 'surya@suryamurugan.in',
   link: 'https://nobelium.vercel.app',
   description: 'This gonna be an awesome website.',
   lang: 'en-US', // ['en-US', 'zh-CN', 'zh-HK', 'zh-TW', 'ja-JP', 'es-ES']
